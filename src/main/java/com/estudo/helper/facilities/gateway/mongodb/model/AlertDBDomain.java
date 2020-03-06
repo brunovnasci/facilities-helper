6d384b58e7145507c22155ecefed9673c4aedab4
Bruno Ventura <brunoventura80@gmail.com>
2020-03-06 11:18:04 -0300

    TimeZone atribuida ao alerta foi modificado

--- src/main/java/com/estudo/helper/facilities/gateway/mongodb/model/AlertDBDomain.java
+++ src/main/java/com/estudo/helper/facilities/gateway/mongodb/model/AlertDBDomain.java
@@ -9,19 +9,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 
 @Document(collection = "alert")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AlertDBDomain {
+    public static final String AMERICA_SAO_PAULO = "America/Sao_Paulo";
     @Id
     private String id;
     private int andar;
     private String generoBanheiro;
     private String mensagem;
     private String person;
-    private Instant dataDeCriacao = LocalDateTime.now().toInstant(OffsetDateTime.now().getOffset());
+    private ZoneId zonedId = ZoneId.of(AMERICA_SAO_PAULO);
+    private Instant dataDeCriacao = LocalDateTime.now().toInstant(OffsetDateTime.now().toLocalDateTime().atZone(zonedId).getOffset());
     private boolean estaFeita = false;
     private Instant dataDeConclusao = null;
 
