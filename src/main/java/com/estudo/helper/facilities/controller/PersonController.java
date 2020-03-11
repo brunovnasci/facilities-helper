65d6cdc3cbb44bd352930249efcc6adcd09308d3
Bruno Ventura <bruno.ventura@zup.com.br>
2020-03-11 10:46:01 -0300

    Metodo getPersonById foi criado!

--- src/main/java/com/estudo/helper/facilities/controller/PersonController.java
+++ src/main/java/com/estudo/helper/facilities/controller/PersonController.java
@@ -1,19 +1,14 @@
 package com.estudo.helper.facilities.controller;
 
 import com.estudo.helper.facilities.controller.annotation.LoginRequired;
-import com.estudo.helper.facilities.controller.exception.GenericServerException;
-import com.estudo.helper.facilities.controller.exception.PersonNotFoundException;
-import com.estudo.helper.facilities.controller.exception.ThePasswordIsWrongException;
+import com.estudo.helper.facilities.controller.exception.*;
 import com.estudo.helper.facilities.controller.mapper.Translator;
 import com.estudo.helper.facilities.controller.model.JwtResponse;
 import com.estudo.helper.facilities.controller.model.PersonRequest;
 import com.estudo.helper.facilities.controller.model.PersonResponse;
 import com.estudo.helper.facilities.entities.Person;
 import com.estudo.helper.facilities.entities.Role;
-import com.estudo.helper.facilities.usecase.CreateNewPersonUseCase;
-import com.estudo.helper.facilities.usecase.GeneratePersonJWTUseCase;
-import com.estudo.helper.facilities.usecase.GetPersonByIdUseCase;
-import com.estudo.helper.facilities.usecase.RemovePersonUseCase;
+import com.estudo.helper.facilities.usecase.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,9 +23,16 @@ public class PersonController {
     private final RemovePersonUseCase removePersonUseCase;
     private final GeneratePersonJWTUseCase generatePersonJWTUseCase;
     private final GetPersonByIdUseCase getPersonById;
+    private final ValidateTokenUseCase validateTokenUseCase;
 
     @GetMapping("/{id}")
     public ResponseEntity<PersonResponse> getPerson(@PathVariable(value = "id") String id, @RequestHeader("Authorization") String jwt) throws PersonNotFoundException {
+        return new ResponseEntity<>(getPersonById.execute(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<PersonResponse> getPersonByJwt(@RequestHeader("Authorization") String jwt) throws PersonNotFoundException, ExpiredJwtException, NotAuthorizedException, GenericServerException, TokenException {
+        String id = validateTokenUseCase.validate(jwt);
         return new ResponseEntity<>(getPersonById.execute(id), HttpStatus.OK);
     }
 
